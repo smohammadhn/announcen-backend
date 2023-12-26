529951dfc42efdf1efd1a1254dff3651b95ee4ba
smohammadhn <smohammadhnd@gmail.com>
2023-12-26 22:21:08 +0330

    feat(user route): add put method

--- routes/users.ts
+++ routes/users.ts
@@ -1,28 +1,36 @@
-import express, { Request, Response } from 'express'
-import User, { validateUser, UserDocument } from '../models/user'
-import { errorMessage } from '../helpers/core'
-import _ from 'lodash'
 import bcrypt from 'bcrypt'
+import express, { Response } from 'express'
+import _ from 'lodash'
+import { errorMessage } from '../helpers/core'
+import auth from '../middlewares/auth'
+import User, { UserDocument, validateLogin, validateUser } from '../models/user'
 
 const router = express.Router()
 
 // register user
 router.post('/', async ({ body }: { body: UserDocument }, res: Response) => {
-  if (!validateUser(body, res)) return
+  if (!validateLogin(body, res)) return
 
   // make sure there is no user with the same email in the database
   const foundUser = await User.findOne({ email: body.email })
-  if (foundUser)
-    return res.status(400).send(errorMessage('User already registered.'))
+  if (foundUser) return res.status(400).send(errorMessage('User already registered.'))
 
   const incomingItem = new User(_.pick(body, ['email', 'password']))
 
-  if (incomingItem.password)
-    incomingItem.password = await bcrypt.hash(incomingItem.password, 10)
+  if (incomingItem.password) incomingItem.password = await bcrypt.hash(incomingItem.password, 10)
 
-  await incomingItem
-    .save()
-    .then((result) => res.send(_.omit(result.toObject(), ['password'])))
+  await incomingItem.save().then((result) => res.send(_.omit(result.toObject(), ['password'])))
+})
+
+// update user
+router.put('/update', auth, async (req: CustomRequest, res: Response) => {
+  if (!validateUser(req.body, res)) return
+
+  await User.findByIdAndUpdate(req.userId, req.body).then((result) => {
+    if (!result) return res.status(404).send('User with the given id not found!')
+
+    res.send(_.omit(result.toObject(), ['password']))
+  })
 })
 
 export = router
